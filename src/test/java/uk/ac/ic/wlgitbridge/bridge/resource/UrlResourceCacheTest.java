cc5396d1e24ec76816e2a0daa69362ab352e8d1f
Winston Li <m@winston.li>
2017-08-13 17:09:15 +0100

    Add ning http headers class

--- src/test/java/uk/ac/ic/wlgitbridge/bridge/resource/UrlResourceCacheTest.java
+++ src/test/java/uk/ac/ic/wlgitbridge/bridge/resource/UrlResourceCacheTest.java
@@ -1,13 +1,19 @@
 package uk.ac.ic.wlgitbridge.bridge.resource;
 
+import com.ning.http.client.HttpResponseHeaders;
 import org.junit.Test;
 import uk.ac.ic.wlgitbridge.bridge.db.noop.NoopDbStore;
+import uk.ac.ic.wlgitbridge.git.exception.SizeLimitExceededException;
 import uk.ac.ic.wlgitbridge.io.http.ning.NingHttpClientFacade;
+import uk.ac.ic.wlgitbridge.io.http.ning.NingHttpHeaders;
+import uk.ac.ic.wlgitbridge.util.FunctionT;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Optional;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UrlResourceCacheTest {
 
@@ -22,20 +28,26 @@ public class UrlResourceCacheTest {
     private final UrlResourceCache cache
             = new UrlResourceCache(new NoopDbStore(), http);
 
+    private static HttpResponseHeaders withContentLength(long cl) {
+        return NingHttpHeaders
+                .builder()
+                .addHeader("Content-Length", String.valueOf(cl))
+                .build();
+    }
+
     @Test
     public void getThrowsSizeLimitWhenContentLengthTooBig() throws Exception {
-//        when(http.get(any(), any())).thenAnswer(new Answer<byte[]>() {
-//            @Override
-//            public byte[] answer(InvocationOnMock invocationOnMock) throws Throwable {
-//                Object[] args = invocationOnMock.getArguments();
-//                ((FunctionT<
-//                        HttpResponseHeaders, Boolean, SizeLimitExceededException
-//                >) args[1]).apply()
-//            }
-//        });
+        when(http.get(any(), any())).thenAnswer(invoc -> {
+            Object[] args = invoc.getArguments();
+            //noinspection unchecked
+            ((FunctionT<
+                    HttpResponseHeaders, Boolean, SizeLimitExceededException
+            >) args[1]).apply(withContentLength(2));
+            return new byte[0];
+        });
         cache.get(
                 PROJ, URL, NEW_PATH,
-                Collections.emptyMap(), Collections.emptyMap(),
+                new HashMap<>(), new HashMap<>(),
                 Optional.of(2L)
         );
     }
