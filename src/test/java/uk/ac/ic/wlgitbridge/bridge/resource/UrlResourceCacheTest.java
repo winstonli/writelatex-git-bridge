7e0b328ee1c931664deb0779ab25ecc002ce0e91
Winston Li <m@winston.li>
2017-08-13 17:26:54 +0100

    Add test

--- src/test/java/uk/ac/ic/wlgitbridge/bridge/resource/UrlResourceCacheTest.java
+++ src/test/java/uk/ac/ic/wlgitbridge/bridge/resource/UrlResourceCacheTest.java
@@ -38,24 +38,36 @@ public class UrlResourceCacheTest {
                 .build();
     }
 
-    private void respondWithContentLength(long cl) throws ExecutionException {
+    private void respondWithContentLength(long cl, long actual)
+            throws ExecutionException {
         when(http.get(any(), any())).thenAnswer(invoc -> {
             Object[] args = invoc.getArguments();
             //noinspection unchecked
             ((FunctionT<
                     HttpResponseHeaders, Boolean, SizeLimitExceededException
             >) args[1]).apply(withContentLength(cl));
-            return new byte[CastUtil.assumeInt(cl)];
+            return new byte[CastUtil.assumeInt(actual)];
         });
+    }
+
+    private void respondWithContentLength(long cl) throws ExecutionException {
+        respondWithContentLength(cl, cl);
+    }
+
+    private void getWithMaxLength(Optional<Long> max)
+            throws IOException, SizeLimitExceededException {
+        cache.get(
+                PROJ, URL, NEW_PATH, new HashMap<>(), new HashMap<>(), max);
     }
 
     private void getWithMaxLength(long max)
             throws IOException, SizeLimitExceededException {
-        cache.get(
-                PROJ, URL, NEW_PATH,
-                new HashMap<>(), new HashMap<>(),
-                Optional.of(max)
-        );
+        getWithMaxLength(Optional.of(max));
+    }
+
+    private void getWithoutLimit()
+            throws IOException, SizeLimitExceededException {
+        getWithMaxLength(Optional.empty());
     }
 
     @Test
@@ -81,6 +93,12 @@ public class UrlResourceCacheTest {
     public void getWithEmptyContentIsValid() throws Exception {
         respondWithContentLength(0);
         getWithMaxLength(0);
+    }
+
+    @Test
+    public void getWithoutLimitDoesNotThrow() throws Exception {
+        respondWithContentLength(Integer.MAX_VALUE, 0);
+        getWithoutLimit();
     }
 
 }
